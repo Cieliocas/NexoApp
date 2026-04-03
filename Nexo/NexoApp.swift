c38694c548c20eeb8d327d0b6c6226da68e29b45
cieliocas <francielioevangelista@hotmail.com>
2026-04-03 19:04:59 -0300

    Initial Commit

---
+++ Nexo/NexoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexoApp.swift
+//  Nexo
+//
+//  Created by Franciélio Castro on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NexoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
